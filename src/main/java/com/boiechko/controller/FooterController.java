e91bb817b7f57c63f77c461e1e82a68f605a46c2
Volodymyr <boiechko17@gmail.com>
2020-09-15 23:02:30 +0300

    added FooterController

---
+++ src/main/java/com/boiechko/controller/FooterController.java
@@ -0,0 +1,35 @@
+package com.boiechko.controller;
+
+import com.boiechko.model.User;
+import com.boiechko.service.implementations.JavaMailService;
+import org.apache.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class FooterController {
+
+    private final Logger logger = Logger.getLogger(FooterController.class);
+
+    @PostMapping("/footer")
+    public ResponseEntity<Object> sendQuestion(@RequestParam("firstName") final String firstName,
+                                               @RequestParam("surname") final String surname,
+                                               @RequestParam("email") final String email,
+                                               @RequestParam("phoneNumber") final String phoneNumber,
+                                               @RequestParam("comment") final String comment) {
+
+        final User user = new User(firstName, surname, email, phoneNumber);
+
+            JavaMailService.sendQuestionFromUserEmail("boiechko.work@gmail.com",
+                "questionFromUser", user, comment);
+
+        logger.info("Question letter has been sent");
+
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+
+    }
+
+}
